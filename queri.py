5df458a974350e4eedbcf4ddb6d3e32ea1cbea9c
Kostadinka <kokitrajanova@yahoo.com>
2026-07-03 19:41:17 +0200

    novi fajlovi

--- queri.py
+++ queri.py
@@ -1,7 +1,8 @@
 
 from baza import engine, Users, Products
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select 
+from sqlalchemy import delete
 
 #session = Session(engine)
 #.....
@@ -34,9 +35,52 @@ from sqlalchemy import select
     
 
 
+# with Session(engine) as session:
+#     users_select = select(Users)
+#     users = session.scalars(users_select).all()
+#     #print(users[0].name)
+#     for user in users:
+#         print(f"{user.id}. {user.name} - {user.email}")
+
+
+# user_id = int(input("Vnesete go redniot broj na korisnikot sto sakate da go izbrisete: "))
+
+# with Session(engine) as session:
+#     users_delete = delete(Users).where(Users.id == user_id)
+#     print(users_delete)
+#     result = session.execute(users_delete)
+#     session.commit()
+
+# user_id = int(input("Vnesete go redniot broj na korisnikot sto sakate da go izbrisete: "))
+
+# with Session(engine) as session:
+#     users_delete = delete(Users).where(Users.id.between(5,25))
+#     print(users_delete)
+#     result = session.execute(users_delete)
+#     session.commit()
+
+# with Session(engine) as session:
+#     users_delete = delete(Users).where(Users.id.between(27, 39))
+#     session.execute(users_delete)
+#     session.commit()
+
+# print("✅ Deleted users from 27 to 39")
+
 with Session(engine) as session:
-    users_select = select(Users)
-    users = session.scalars(users_select).all()
-    #print(users[0].name)
-    for user in users:
-        print(f"{user.id}. {user.name} - {user.email}")
+    user = Users(name="Ana", email="ana@example.com") # INSERT INTO User (name,email) VALUES ("Ana", "ana@example.com");
+    session.add(user)
+    session.commit()
+    for i in range(5):
+         ime = input("Vnesi Ime: ")
+         email = input("Vnesi email: ")
+         user = Users(name=ime, email=email)
+         session.add(user)
+         session.commit()
+
+users_list = []
+for i in range(5):
+     user_dict = {}
+     user_dict["ime"] = input("Vnesi Ime: ")
+     user_dict["email"] = input("Vnesi email: ")
+     users_list.append(user_dict)
+     #[{"ime":"Ana", "email":"ana@example.com"},......]
